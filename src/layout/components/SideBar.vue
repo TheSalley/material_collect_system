3d7901824b94f8452758d52a26dbc9865b6799c5
faiz <1919532973@qq.com>
2025-11-10 18:16:37 +0800

    调整

--- src/layout/components/SideBar.vue
+++ src/layout/components/SideBar.vue
@@ -1,43 +1,29 @@
 <template>
   <div class="aside-container">
-    <img class="logo" src="/logo.webp" />
-    <el-menu
-      router
-      default-active="1"
-      text-color="#FFF"
-      class="flex-1"
-      @open="handleOpen"
-      @close="handleClose"
-    >
+    <img class="logo" src="/logo.svg" />
+    <el-menu router default-active="1" class="flex-1">
       <el-menu-item index="1" route="/">运营</el-menu-item>
       <el-menu-item index="2" route="/admin">客户</el-menu-item>
     </el-menu>
   </div>
 </template>
 
-<script setup>
-import {
-  Document,
-  Menu as IconMenu,
-  Location,
-  Setting,
-} from "@element-plus/icons-vue";
-
-const handleOpen = (key, keyPath) => {
-  console.log(key, keyPath);
-};
-const handleClose = (key, keyPath) => {
-  console.log(key, keyPath);
-};
-</script>
+<script setup></script>
 <style scoped>
 .aside-container {
   display: flex;
   flex-direction: column;
   --el-menu-bg-color: #001428;
+  --el-menu-text-color: #fff;
+  --el-menu-active-color: #409eff;
 }
 .logo {
+  padding-inline: 10px;
   height: 85px;
-  object-fit:contain;
+  object-fit: contain;
+}
+
+.el-menu-item:hover {
+  color: #409eff;
 }
 </style>
