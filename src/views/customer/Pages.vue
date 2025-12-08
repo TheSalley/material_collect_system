9760cd57b6ce3060db0f74a9a49795e7093e1e24
The Shire <16150968+TheSalley@user.noreply.gitee.com>
2025-12-08 23:59:41 +0800

    UI 优化

--- src/views/customer/Pages.vue
+++ src/views/customer/Pages.vue
@@ -1,3 +1,25 @@
 <template>
-    网站页面列表，待开发
+    <el-tabs class="w-full" v-model="activePage" v-if="pageList.length">
+        <el-tab-pane :label="page.post_name" :name="page.post_name" v-for="page in pageList" :key="page.id">
+            <template v-if="user.mode === 1">
+                <TW :pageId="page.id" />
+            </template>
+            <template v-else>
+                <PageMode />
+            </template>
+        </el-tab-pane>
+    </el-tabs>
+    <el-empty description="无可编辑页面" v-else />
 </template>
+<script setup>
+import { ref, reactive } from "vue";
+import { useGlobalStore } from "@/stores/global.js";
+import TW from "@/components/TW.vue";
+import PageMode from "@/components/PageMode.vue";
+
+
+const activePage = ref("home");
+let pageList = reactive([]);
+const { user } = useGlobalStore();
+pageList = JSON.parse(user.page_list);
+</script>
