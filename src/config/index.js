9d3b57fb08c0eba96ffe2670235fa61c42ab487e
faiz <1919532973@qq.com>
2026-04-10 18:41:18 +0800

    language 放到config 里

--- src/config/index.js
+++ src/config/index.js
@@ -13,4 +13,19 @@ const APP_VERSION = "0.1.6";
 /** 是否启用严格尺寸限制：开启后，选图后确定的尺寸即为唯一允许上传的尺寸 */
 const STRICT_IMAGE_DIMENSION_ENABLED = false;
 
-export { APP_VERSION, STRICT_IMAGE_DIMENSION_ENABLED };
+
+const languageList = [
+    { label: "中文", value: "zh" },
+    { label: "英语", value: "en" },
+    { label: "俄语", value: "ru" },
+    { label: "法语", value: "fr" },
+    { label: "德语", value: "de" },
+    { label: "意大利语", value: "it" },
+    { label: "西班牙语", value: "es" },
+    { label: "葡萄牙语", value: "pt" },
+    { label: "阿拉伯语", value: "ar" },
+    { label: "日语", value: "ja" },
+    { label: "韩语", value: "ko" },
+];
+
+export { APP_VERSION, STRICT_IMAGE_DIMENSION_ENABLED, languageList };
